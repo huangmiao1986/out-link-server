65f052760f68ce7b57dda483d112ff0aa6d942f2
huangmiao <huangmiao@huangmiao>
2015-03-16 11:36:18 +0800

    修正

--- src/main/java/com/out/link/server/websocket/servlet/WebSocketMessageServlet.java
+++ src/main/java/com/out/link/server/websocket/servlet/WebSocketMessageServlet.java
@@ -1,10 +1,9 @@
 package com.out.link.server.websocket.servlet;
 
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.catalina.websocket.StreamInbound;
-@WebServlet(name="websocket",urlPatterns="/message")
+
 public class WebSocketMessageServlet extends org.apache.catalina.websocket.WebSocketServlet{
 
 	private static final long serialVersionUID = -5266831343193318242L;
